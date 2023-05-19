f06321f42228ba737a114d25d6847de8c10c74e3
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-19 14:42:25 -0600

    :sparkles: Add auth controller and services
    It was added the auth controller and its dtos, and its services.
    
    Co-authored-by: Fernando Melendez 00018720@uca.edu.sv

--- Server/src/main/java/com/group6/server/controllers/AuthController.java
+++ Server/src/main/java/com/group6/server/controllers/AuthController.java
@@ -1,8 +1,11 @@
 package com.group6.server.controllers;
 
+import com.group6.server.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,21 @@ import com.group6.server.models.dtos.MessageDTO;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-	
-	@GetMapping("/login")
+	@Autowired
+	private AuthService userService;
+
+
+
+
+	@PostMapping("/login")
 	public ResponseEntity<?> getLogin(){
 		return new ResponseEntity<Object>( new MessageDTO("Controller working successfully"), 
 				HttpStatus.OK);
 	}
-	
+
+	@PostMapping("/google")
+	public ResponseEntity<?> getRegister(){
+		return new ResponseEntity<Object>( new MessageDTO("Controller working successfully"),
+				HttpStatus.OK);
+	}
 }
---
+++ Server/src/main/java/com/group6/server/models/entites/User.java
@@ -0,0 +1,17 @@
+package com.group6.server.models.entites;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+}
