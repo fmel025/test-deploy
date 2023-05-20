72a7e0ff0da2f676d19124d7aacba25af0cde363
ChristopherCalderon <chris2001289@gmail.com>
2023-05-19 22:46:39 -0600

    :lipstick: Add linechart to statistic
    Added Scanned ticket per hour chart
    
    Co-authored-by: Christopher Calderon 00069920@uca.edu.sv

--- Client/src/pages/EventStatistics/EventStatistics.jsx
+++ Client/src/pages/EventStatistics/EventStatistics.jsx
@@ -1,17 +1,19 @@
 
 import EventStatsTable from "../../components/EventStatsTable/EventStatsTable"
 import Layout from "../../components/Layout/Layout"
+import StatisticsChart from "../../components/StatisticsChart/StatisticsChart"
 
 
 function EventStatistics() {
  
     return (
         <Layout>
-          <div className="bg-white flex flex-col gap-1 h-auto w-10/12 rounded-lg shadow-lg p-8 m-10 ">
-            <h1 className="text-2xl font-bold ">Titulo del evento</h1>
-            <p className="font-light text-gray-500">Fecha</p>
-
+        
+        <h1 className="text-2xl font-bold mt-3 ">Titulo del evento</h1>
+        <p className="font-light text-gray-500">Fecha</p>
+          <div className="bg-white flex gap-1 h-auto w-10/12 rounded-lg shadow-lg p-8 m-3 ">
              <EventStatsTable/>
+             <StatisticsChart/>
           </div>
           
         </Layout>
