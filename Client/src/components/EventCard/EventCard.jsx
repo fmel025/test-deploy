be956114ce3d3f73b87325041353a9c5a01782a9
Samuel581 <00023020@uca.edu.sv>
2023-05-20 14:58:01 -0600

    :lipstick: Add responsive behavior to event views
    Added resposive design to home view, atended events and future events
    view

--- Client/src/components/EventCard/EventCard.jsx
+++ Client/src/components/EventCard/EventCard.jsx
@@ -2,7 +2,7 @@ import DetailsButton from "../CounterButtons/CounterButtons"
 
 const EventCard = () => {
     return(
-        <div className='bg-white cursor-pointer w-64 h-92 rounded-lg shadow-lg hover:shadow-2xlBlack'>
+      <div className='bg-white cursor-pointer w-64 h-92 rounded-lg shadow-lg hover:shadow-2xlBlack'>
         <figure className='relative m-3 w-90 h-2/5'>
           <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
         </figure>
@@ -14,7 +14,6 @@ const EventCard = () => {
           <p className='ml-5 text-sm font-light'>69 de mayo</p>
           <p className='mt-2 text-sm font-medium'>Cupos disponibles:</p>
           <p className='ml-5 text-sm font-light'>50</p>
-          
         </div>
       </div>
     )
--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -8,18 +8,18 @@ function Home() {
  
     return (
         <Layout>
-          <div className="w-full h-2/3 flex justify-between p-10">
-            <div className="p-36 w-2/4">
+          <div className="w-screen flex flex-col lg:flex-row justify-between items-center p-10">
+            <div className="w-4/5 text-center">
               <h1 className="text-5xl font-bold ">EventMate</h1>
               <h2 className="text-3xl mt-7">Mas que una experiencia, una vergota, conectate con el mundo</h2>
             </div>
-            <figure className='relative m-3 w-90 h-full w-2/4'>
+            <figure className='relative m-3 w-90 h-full md:w-2/4 w-4/5'>
               <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
             </figure>
           </div>
-          <h1 className="text-5xl font-bold text-main mb-10">Eventos</h1>
+          <h1 className="text-5xl font-bold text-main mb-10 mt-24">Eventos</h1>
           <SearchBar/>
-          <div className="grid grid-cols-4 gap-16 my-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-16 my-5">
           <EventCard/>
           <EventCard/>
           <EventCard/>
