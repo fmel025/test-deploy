5e52e4af7c88fb8c62dc82c845d7c26140fbe6bb
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-20 22:15:34 -0600

    :construction: Add transaction entity attributes
    Five attributes were added to store the information in the DB

--- Server/src/main/java/com/group6/server/models/entites/Transaction.java
+++ Server/src/main/java/com/group6/server/models/entites/Transaction.java
@@ -1,4 +1,15 @@
 package com.group6.server.models.entites;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
 public class Transaction {
+    private String id;
+    private Date date;
+    private String userTempCode;
+    private String confirmationCode;
 }
