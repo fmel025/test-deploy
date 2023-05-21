a10307ede79bfb92083f3805085f22a04a14d338
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-20 22:04:56 -0600

    :construction: add purchase entity attributes
    Added 3 main attributes to reference the database

--- Server/src/main/java/com/group6/server/models/entites/Purchase.java
+++ Server/src/main/java/com/group6/server/models/entites/Purchase.java
@@ -1,4 +1,16 @@
 package com.group6.server.models.entites;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
 public class Purchase {
+
+    private String id;
+    private String total;
+    private Date date;
+
+
 }
