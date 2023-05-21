0b309db6a21839f45e9a12662fcd940a5f60d58f
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-20 22:10:29 -0600

    :construction: Add ticket entity attributes
    Added 5 main attributes to reference the database

--- Server/src/main/java/com/group6/server/models/entites/Ticket.java
+++ Server/src/main/java/com/group6/server/models/entites/Ticket.java
@@ -1,4 +1,17 @@
 package com.group6.server.models.entites;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
 public class Ticket {
+    private String id;
+    private String temporaryCode;
+    private String state;
+    private String price;
+    private Date date;
+
 }
