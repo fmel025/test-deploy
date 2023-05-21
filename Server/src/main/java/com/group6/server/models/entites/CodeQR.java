ca2e2f10cfe589cf0a30cadf812c8e49e2341745
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-20 22:22:03 -0600

    :construction: Add CodeQR entity attributes
    The attributes where the ticket validity time is handled were added

--- Server/src/main/java/com/group6/server/models/entites/CodeQR.java
+++ Server/src/main/java/com/group6/server/models/entites/CodeQR.java
@@ -1,4 +1,15 @@
 package com.group6.server.models.entites;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
 public class CodeQR {
+    private String id;
+    private Date creationDate;
+    private Date expirationDate;
+    private Date validationDate;
 }
