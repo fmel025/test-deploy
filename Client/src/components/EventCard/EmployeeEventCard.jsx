f08512d4e2c65fd4813ecf20ae6da000ca37a0aa
Samuel581 <00023020@uca.edu.sv>
2023-05-31 14:39:17 -0600

    :construction: Add tabs and buttons to icons
    Added icons to buttons in create events form and also implemented tabs
    to all tickets view

--- Client/src/components/EventCard/EmployeeEventCard.jsx
+++ Client/src/components/EventCard/EmployeeEventCard.jsx
@@ -6,19 +6,19 @@ import { IconTicket } from '@tabler/icons-react';
 
 const EventCard = () => {
     return(
-        <Link to='/employee_event_details' className='bg-white cursor-pointer w-64 h-92 border rounded-lg hover:shadow-xl'>
+        <Link to='/employee_event_details' className='bg-white cursor-pointer w-72 h-92 border rounded-lg hover:shadow-xl'>
         <figure className='relative m-3 w-90 h-2/5'>
           <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
         </figure>
         <div className='m-4'>
-          <h1 className='mt-1 text-2xl font-bold'>
-            Fiesta del sexo
+          <h1 className='mt-1 text-xl font-bold'>
+            Concierto Ferxxxo
           </h1>
           <div className="flex flex-row items-center gap-1">
             <IconMapPin size={15} className="mt-1"/>
             <p className='mt-2 text-sm font-medium'>Lugar</p>
           </div>
-          <p className='ml-5 text-sm text-slate-500 truncate'>Estadio Magico Gonzales</p>
+          <p className='ml-5 text-sm text-slate-500 truncate'>Estadio Magico Gonzales, Platea Sur</p>
           <div className="flex flex-row items-center gap-1">
             <IconCalendarEvent size={15} />
             <p className='mt-1 text-sm font-medium'>Fecha</p>
