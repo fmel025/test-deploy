f83baa9d638677e964338f1c8491750758b0498c
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-05-31 11:25:25 -0600

    :card_file_box: Add Authorization and Event entites
     The authorization and event entities were added
    
    Co-Authored-By: MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>

---
+++ Server/src/main/java/com/group6/server/models/entites/Authorization.java
@@ -0,0 +1,13 @@
+package com.group6.server.models.entites;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Authorization {
+
+    String id;
+
+    String name;
+}
