568121fad6c67c417df9dd86e2d01a2a97497643
Samuel581 <00023020@uca.edu.sv>
2023-05-31 13:08:45 -0600

    :construction: Add icons to event card
    Added icons to events cards, improved UI and fixed text overflow

--- Client/src/components/EventCard/EventCard.jsx
+++ Client/src/components/EventCard/EventCard.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom"
+import { IconMapPin } from '@tabler/icons-react';
+import { IconCalendarEvent } from '@tabler/icons-react';
+import { IconTicket } from '@tabler/icons-react';
 
 const EventCard = () => {
     return(
@@ -7,13 +10,25 @@ const EventCard = () => {
           <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
         </figure>
         <div className='m-4'>
-          <h1 className='mt-1 text-2xl font-bold'>Fiesta del sexo</h1>
-          <p className='mt-2 text-sm font-medium'>Lugar</p>
+          <h1 className='mt-1 text-2xl font-bold'>
+            Fiesta del sexo
+          </h1>
+          <div className="flex flex-row items-center gap-1">
+            <IconMapPin size={15} className="mt-1"/>
+            <p className='mt-2 text-md font-medium'>Lugar</p>
+          </div>
           <p className='ml-5 text-sm font-light'>Casa de pupa</p>
-          <p className='mt-1 text-sm font-medium'>Fecha</p>
+          <div className="flex flex-row items-center gap-1">
+            <IconCalendarEvent size={15} />
+            <p className='mt-1 text-sm font-medium'>Fecha</p>
+          </div>
+          
           <p className='ml-5 text-sm font-light'>69 de mayo</p>
-          <p className='mt-2 text-sm font-medium'>Cupos disponibles:</p>
-          <p className='ml-5 text-sm font-light'>50</p>
+          <div className="flex flex-row items-center gap-1">
+            <IconTicket size={15} className="mt-1"/>
+            <p className='mt-2 text-sm font-medium'>Cupos disponibles:</p>
+          </div>
+          <p className='ml-5 text-sm font-light'>50</p>    
         </div>
       </Link>
     )
