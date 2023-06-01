01c412db1a6bb94188ff5d1813896548425a62e9
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-31 23:26:22 -0600

    :construction: Add method to compare passwords

--- Server/src/main/java/com/group6/server/services/UserService.java
+++ Server/src/main/java/com/group6/server/services/UserService.java
@@ -5,4 +5,7 @@ import com.group6.server.models.entites.User;
 public interface UserService {
     //Register method
     User register(RegisterDTO registerDTO);
+
+    //compare password method
+    boolean comparePassword(String password, String hashedPassword);
 }
