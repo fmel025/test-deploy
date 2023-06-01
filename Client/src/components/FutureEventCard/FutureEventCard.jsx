e9238fdb2c57341fb49f1e9da2acb0c95482f979
ChristopherCalderon <chris2001289@gmail.com>
2023-06-01 00:01:08 -0600

    :sparkles: Add generic text to client views
    Added proper static text to client views and components
    
    Co-authored-by: Christopher Calderon 00069920@uca.edu.sv

--- Client/src/components/FutureEventCard/FutureEventCard.jsx
+++ Client/src/components/FutureEventCard/FutureEventCard.jsx
@@ -7,13 +7,13 @@ const FutureEventCard = () => {
           <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
         </figure>
         <div className='m-4'>
-          <h1 className='mt-1 text-2xl font-bold'>Fiesta del sexo</h1>
+        <h1 className='mt-1 text-2xl font-bold'>Titulo</h1>
           <p className='mt-2 text-sm font-medium'>Lugar:</p>
-          <p className='ml-5 text-sm font-light'>Casa de pupa</p>
+          <p className='ml-5 text-sm font-light'>Estadio</p>
           <p className='mt-1 text-sm font-medium'>Fecha:</p>
-          <p className='ml-5 text-sm font-light'>69 de mayo</p>
+          <p className='ml-5 text-sm font-light'>23 de mayo</p>
           <p className='mt-2 text-sm font-medium'>Localidad:</p>
-          <p className='ml-5 text-sm font-light'>Cama VIP</p>
+          <p className='ml-5 text-sm font-light'>VIP</p>
           
         </div>
       </Link>
--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
           <div className="w-screen flex flex-col lg:flex-row justify-between items-center p-10">
             <div className="w-4/5 text-center">
               <h1 className="text-5xl font-bold ">EventMate</h1>
-              <h2 className="text-3xl mt-7">Mas que una experiencia, una vergota, conectate con el mundo</h2>
+              <h2 className="text-3xl mt-7">Mas que un evento, es una experiencia, conectate con el mundo</h2>
             </div>
             <figure className='relative m-3 w-90 h-full md:w-2/4 w-4/5'>
               <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
