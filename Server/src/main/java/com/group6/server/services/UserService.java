2dd5be2813d85d8b73fa6d57aebc78d9f6e35d73
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-31 23:21:44 -0600

    :construction:: Add method to register an user

--- Server/src/main/java/com/group6/server/services/UserService.java
+++ Server/src/main/java/com/group6/server/services/UserService.java
@@ -1,4 +1,8 @@
 package com.group6.server.services;
 
+import com.group6.server.models.entites.User;
+
 public interface UserService {
+    //Register method
+    User register(RegisterDTO registerDTO);
 }
