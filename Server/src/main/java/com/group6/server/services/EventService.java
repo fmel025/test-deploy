630de38c3287d8c57ee460d82f7c39b58dfa300e
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-05-31 23:57:06 -0600

    :construction: Add method to create an event

--- Server/src/main/java/com/group6/server/services/EventService.java
+++ Server/src/main/java/com/group6/server/services/EventService.java
@@ -1,4 +1,9 @@
 package com.group6.server.services;
 
+import com.group6.server.models.entites.Event;
+
 public interface EventService {
+    //create event
+    Event createEvent(EventDTO eventDTO);
+
 }
