efece5caada1f6369f667e48767f8768e8e69cc0
Samuel581 <00023020@uca.edu.sv>
2023-06-04 16:57:47 -0600

    :lipstick: Update add new event button
    Improved the button that adds a new event and added responsive behavior

--- Client/src/components/AddButton/AddButton.jsx
+++ Client/src/components/AddButton/AddButton.jsx
@@ -1,16 +1,15 @@
 import { useNavigate } from 'react-router-dom';
-import { IconCirclePlus } from '@tabler/icons-react';
+import {IconTextPlus} from '@tabler/icons-react';
+
 
 export default function AddButton() {
       const navigate = useNavigate();
 
-    return (
-        <div className="relative mb-4 items-center">
-        <button onClick={() => navigate('/create_event')} className="btn btn-sm bg-main hover:bg-darker-main btn-square">
-              <IconCirclePlus />
-        </button>
-  </div>
-        
-        );
-  }
-  
+      return (
+            <button onClick={() => navigate('/create_event')} className="btn md:btn-sm md:mt-0.5 bg-main hover:bg-darker-main text-slate-100">
+                  <IconTextPlus/>
+                  <span>Nuevo evento</span>
+            </button>
+
+      );
+}
--- Client/src/pages/EventStatisticsSelection/EventStatisticsSelection.jsx
+++ Client/src/pages/EventStatisticsSelection/EventStatisticsSelection.jsx
@@ -5,21 +5,20 @@ import StatsCard from "../../components/StatsCard/StatsCard"
 
 
 function EventStatisticsSelection() {
- 
-    return (
-        <Layout>
-          <SearchBar />
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-16 mb-5 ">
-          <StatsCard />
-          <StatsCard />
-          <StatsCard />
-          <StatsCard />
-          <StatsCard />
-          </div>
-          
-        </Layout>
-    )
-  }
-  
-  export default EventStatisticsSelection
-  
+
+  return (
+    <Layout>
+      <SearchBar />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-16 mb-5 ">
+        <StatsCard />
+        <StatsCard />
+        <StatsCard />
+        <StatsCard />
+        <StatsCard />
+      </div>
+
+    </Layout>
+  )
+}
+
+export default EventStatisticsSelection
