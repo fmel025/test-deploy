19d9d458cdc940122aabc14300ec9213838ee5a0
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-06-04 15:57:33 -0600

    :lipstick: Improve login UI
    The login page design had several UI changes for improvement
    
    Co-Authored-By: Samuel Ortiz <62672937+Samuel581@users.noreply.github.com>

--- Client/src/pages/Login/Login.jsx
+++ Client/src/pages/Login/Login.jsx
@@ -1,35 +1,86 @@
-
-import Layout from '../../components/Layout/Layout'
-import { UimGoogle } from '@iconscout/react-unicons-monochrome'
-import { useNavigate } from 'react-router-dom';
+import Layout from "../../components/Layout/Layout";
+import { UimGoogle } from "@iconscout/react-unicons-monochrome";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/google.svg";
 
 function Login() {
   const navigate = useNavigate();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  const handleSubmitGoogle = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
   return (
-    <Layout>
-    
-    <div className="rounded w-4/5 md:w-3/5 lg:w-2/5 flex-col justify-center p-10 mt-10 md:mt-28 bg-white">
-    {/* <h1 className="text-5xl font-bold text-main mb-10  ">EventMate</h1> */}
-        <h1 className='font-semibold text-3xl text-center mb-10'>Incio de sesion</h1>
-        <p className='font-semibold'>Correo electronico:</p>
-        <input type='text' className='input w-full input-sm bg-slate-200' placeholder='example@gmail.com'></input>
-        <p className='font-semibold'>Contraseña:</p>
-        <input type='password' className='input w-full input-sm bg-slate-200' placeholder='********'></input>
-        <div className='flex flex-col justify-center items-center my-5'>
-        <div onClick={() => navigate('/')} className='btn bg-main hover:bg-darker-main'>Iniciar sesion</div> 
-        <div className='divider'>o</div>         
+    <div className="flex items-center justify-center min-h-screen bg-violet-100">
+      <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+        <form className="flex flex-col justify-center p-8 md:p-14">
+          <span className="mb-3 text-3xl md:text-4xl font-bold">Iniciar sesion</span>
+          <span className="font-light text-gray-400 mb-4">
+            Bienvenido de nuevo a EventMate
+          </span>
+          <div className="py-4">
+            <label htmlFor="email" className="mb-2 text-md">
+              Email o Username
+            </label>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+              name="email"
+              id="email"
+              placeholder="example@gmail.com"
+            />
+          </div>
+          <div className="py-4">
+            <label htmlFor="password" className="mb-2 text-md">Contraseña</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="********"
+              className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+            />
+          </div>
+
+          <button
+            className="w-full text-sm bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300"
+            type="button"
+            onClick={handleSubmit}
+          >
+            Iniciar sesion
+          </button>
+
+          <div className="py-4 flex items-center justify-center">
+            <div className="border-t border-gray-300 w-1/3"></div>
+            <div className="mx-3 text-gray-500">O</div>
+            <div className="border-t border-gray-300 w-1/3"></div>
+          </div>
+
+          <button
+            className="w-full border border-gray-300 text-sm p-2 rounded-lg mb-6 hover:bg-black hover:text-white"
+            type="button"
+            onClick={handleSubmitGoogle}
+          >
+            <img src={Logo} alt="logo" class="w-6 h-6 inline mr-2" />
+            Iniciar sesion con Google
+          </button>
+        </form>
+
+        <div className="relative">
+          <img
+            src="https://images.unsplash.com/photo-1684235424737-31c4f78e5aa2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+            alt="img"
+            className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
+          />
         </div>
-        {/* Google login button */}
-        <div className='flex flex-col justify-center items-center'>
-            <button  className='btn btn-outline' onClick={() => navigate('/')}>
-              <UimGoogle className='mr-2 text-main'/>
-              Iniciar sesion con Google
-            </button>
-        </div>
+      </div>
     </div>
-    </Layout>
-  )
+  );
 }
 
-export default Login
+export default Login;
