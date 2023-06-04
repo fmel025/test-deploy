b0477825a57552bed6cc0ca87fb97f4ff8a0ac9b
ChristopherCalderon <chris2001289@gmail.com>
2023-06-04 13:35:51 -0600

    :sparkles: Add responsiveness to event details
    Make event details for client and admin responsive for all devices
    
    Co-authored-by: Christopher Calderon 00069920@uca.edu.sv

--- Client/src/pages/EventDetails/EventDetails.jsx
+++ Client/src/pages/EventDetails/EventDetails.jsx
@@ -21,7 +21,7 @@ function EventDetails() {
             <p className="font-bold">Patrocinadores</p>
             <p className="font-bold">Cupos disponibles</p>
           </div>
-          <div className="grid grid-cols-4 gap-16 mb-10 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-16 mb-10 ">
           <TierCard/>
           <TierCard/>
           <TierCard/>
