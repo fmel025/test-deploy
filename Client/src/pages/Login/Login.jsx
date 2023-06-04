896b54fc05ded90efe402202ed431f99aa17ef8b
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-06-04 15:59:25 -0600

    :construction: Add states to login
    In the login page was added the email and password state to handle the user data.
    
    Co-Authored-By: Samuel Ortiz <62672937+Samuel581@users.noreply.github.com>

--- Client/src/pages/Login/Login.jsx
+++ Client/src/pages/Login/Login.jsx
@@ -4,6 +4,10 @@ import { useNavigate } from "react-router-dom";
 import Logo from "../../assets/google.svg";
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
