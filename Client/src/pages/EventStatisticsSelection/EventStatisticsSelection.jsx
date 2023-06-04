20cbb197d0a1d6345a2351a2dd61629175550780
ChristopherCalderon <chris2001289@gmail.com>
2023-06-04 13:25:19 -0600

    :sparkles: Add responsiveness to event selection
    Make event selection responsive for all devices
    
    Co-authored-by: Christopher Calderon 00069920@uca.edu.sv

--- Client/src/pages/EventStatisticsSelection/EventStatisticsSelection.jsx
+++ Client/src/pages/EventStatisticsSelection/EventStatisticsSelection.jsx
@@ -9,7 +9,7 @@ function EventStatisticsSelection() {
     return (
         <Layout>
           <SearchBar />
-          <div className="grid grid-cols-3 gap-16 mb-5 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-16 mb-5 ">
           <StatsCard />
           <StatsCard />
           <StatsCard />
