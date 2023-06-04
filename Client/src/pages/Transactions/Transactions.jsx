ad61174e9dbb2ffc3333521f1a66eb14447f3691
Samuel581 <00023020@uca.edu.sv>
2023-06-04 10:43:41 -0600

    :lipstick: Add tabs to transactions view
    Added tabbed behavior to all transactions view, now it shows one section and swaps to another when you click on it's tab

--- Client/src/pages/Transactions/Transactions.jsx
+++ Client/src/pages/Transactions/Transactions.jsx
@@ -2,38 +2,92 @@
 import IssuedCard from "../../components/IssuedCard/IssuedCard"
 import ReceivedCard from "../../components/ReceivedCard/ReceivedCard"
 import Layout from "../../components/Layout/Layout"
+import React from "react";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+import TransactionLayout from "../../components/Layout/TransactionLayout"
+import { IconArrowAutofitUp, IconArrowAutofitDown } from "@tabler/icons-react";
+
 
 
 function Transactions() {
-return (
+  const [activeTab, setActiveTab] = React.useState("html");
+  const data = [
+    {
+      label: "Transferencias recibidas",
+      value: "recived",
+      icon: IconArrowAutofitDown,
+      content: <TransactionLayout>
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+        <ReceivedCard />
+      </TransactionLayout>
+    },
+    {
+      label: "Transferencias emitidas",
+      value: "issued",
+      icon: IconArrowAutofitUp,
+      content: <TransactionLayout>
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        <IssuedCard />
+        
+        </TransactionLayout>
+    }
+  ]
+
+
+  return (
     <Layout>
       <h1 className="text-5xl font-bold text-blue-600 mt-5 mb-10">Transferencias</h1>
-      <div className="flex gap-10 w-4/5 h-5/12 overflow-x-auto">
-        <div className="w-{360} flex flex-col  items-center max-h-full">
-            <h1 className="text-blue-600 font-bold text-lg">Transferencias emitidas</h1>
-            <div className="mt-5 mb-5 flex flex-col  h-3/4 gap-y-7 overflow-auto scrollbar-thin scrollbar-thumb-slate-500 scrollbar-track-gray-300">
-              <IssuedCard/>
-              <IssuedCard/>
-              <IssuedCard/>
-              <IssuedCard/>
-              <IssuedCard/>
-              <IssuedCard/>
-            </div>
-        </div>
-        <div className="w-{360} flex flex-col items-center max-h-full">
-            <h1 className="text-blue-600 font-bold text-lg">Transferencias recibidas</h1>
-            <div className="mt-5 mb-5 flex flex-col  h-3/4 gap-y-7 overflow-auto scrollbar-thin scrollbar-thumb-slate-500 scrollbar-track-gray-300">
-              <ReceivedCard/>
-              <ReceivedCard/>
-              <ReceivedCard/>
-              <ReceivedCard/>
-            </div>
-        </div>
-      </div>
-      
+      <Tabs value={activeTab} className="w-2/3">
+        <TabsHeader className="bg-slate-200 p-2 mx-10 text-center">
+          {data.map(({ label, value, icon }) => (
+            <Tab
+              key={value}
+              value={value}
+              onClick={() => setActiveTab(value)}
+              className={activeTab === value ? "text-blue-500" : ""}>
+              <div className="flex items-center gap-2">
+                {React.createElement(icon, { className: "w-5 h-5" })}
+                {label}
+              </div>
+            </Tab>
+          ))}
+        </TabsHeader>
+        {/* Make ir scrollable*/}
+        <TabsBody className="items-center m-auto">
+          {data.map(({ value, content }) => (
+            <TabPanel key={value} value={value}>
+              {content}
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
+
     </Layout>
-)
+  )
 }
 
 export default Transactions
-  
