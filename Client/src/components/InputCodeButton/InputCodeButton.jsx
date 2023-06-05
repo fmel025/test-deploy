af97f5279135187b8d6c0fa2104354e6f21ebcfa
ChristopherCalderon <chris2001289@gmail.com>
2023-06-05 01:50:56 -0600

    :sparkles: Add input transaction code view
    Added view for transaction code with responsiveness
    
    Co-Authored-By: Samuel Ortiz <62672937+Samuel581@users.noreply.github.com>

---
+++ Client/src/components/InputCodeButton/InputCodeButton.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom'; 
+function InputCodeButton() {
+  const navigate = useNavigate();
+  return (
+    <div>
+        <button className="btn bg-main px-10 hover:bg-darker-main font-bold text-white" onClick={()=>window.my_modal_5.showModal()}>Aceptar</button>
+        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+          <form method="dialog" className="modal-box">
+            <h3 className="font-bold text-lg">Confirmar transferencia</h3>
+            <p className="py-4">Ingrese codigo de confirmacion</p>
+            <input type='text' className='input-bordered input bg-slate-200 w-4/5' placeholder='Codigo de usuario'></input>
+                   
+            <div className="modal-action">
+              <button className="btn bg-red-500 hover:bg-red-700 font-bold text-white">Cerrar</button>
+              <button onClick={() => navigate('/transaction_details')} className="btn  bg-main hover:bg-darker-main font-bold text-white">Aceptar</button>
+            </div>
+          </form>
+        </dialog>
+    </div>
+  )
+}
+
+export default InputCodeButton
