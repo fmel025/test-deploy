836b84d2af8ac322fcab368fd003ed2fb03b23b4
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-06-05 14:59:51 -0600

    :bug: Add create the event DTO
    In event service the event dto was required and it did not exist before.
    
    Co-Authored-By: MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>

--- Server/src/main/java/com/group6/server/utils/Constants.java
+++ Server/src/main/java/com/group6/server/utils/Constants.java
@@ -4,5 +4,11 @@ public class Constants {
 
     public static final String API_BASE_URL = "/api/v1";
 
+    // For all the workers
+    public static final String API_ADMIN_URL = API_BASE_URL + "/admin";
+
+    // Available for everyone
+    public static final String API_USER_URL = API_BASE_URL + "/public";
+
     private Constants(){}
 }
