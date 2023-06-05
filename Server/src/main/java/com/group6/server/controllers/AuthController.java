0671f54feeee8f1af7cb79a0d7a9a2eeaa3ab9cc
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-06-05 14:34:49 -0600

    :construction: Add validations to auth routes
    The routes from auth have now implemented the validations of its dto
    
    Co-Authored-By: MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>

--- Server/src/main/java/com/group6/server/controllers/AuthController.java
+++ Server/src/main/java/com/group6/server/controllers/AuthController.java
@@ -1,33 +1,42 @@
 package com.group6.server.controllers;
 
+import com.group6.server.models.dtos.SignInDTO;
+import com.group6.server.models.dtos.SignInGoogleDTO;
 import com.group6.server.services.AuthService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import com.group6.server.utils.Constants;
 
 import com.group6.server.models.dtos.MessageDTO;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping(Constants.API_BASE_URL + "/auth")
 public class AuthController {
+
 	@Autowired
 	private AuthService userService;
 
-
-
-
 	@PostMapping("/login")
-	public ResponseEntity<?> getLogin(){
+	public ResponseEntity<?> getLogin(@Valid @RequestBody SignInDTO data, BindingResult validations){
+
+		if(validations.hasErrors()){
+			return ResponseEntity.badRequest().body("Bad req");
+		}
 		return new ResponseEntity<Object>( new MessageDTO("Controller working successfully"), 
 				HttpStatus.OK);
 	}
 
 	@PostMapping("/google")
-	public ResponseEntity<?> getRegister(){
+	public ResponseEntity<?> getGoogleLogin(@Valid @RequestBody SignInGoogleDTO data, BindingResult validations) {
+
+		if(validations.hasErrors()){
+			return ResponseEntity.badRequest().body("Bad req");
+		}
+
 		return new ResponseEntity<Object>( new MessageDTO("Controller working successfully"),
 				HttpStatus.OK);
 	}
---
+++ Server/src/main/java/com/group6/server/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.group6.server.utils;
+
+public class Constants {
+
+    public static final String API_BASE_URL = "/api/v1";
+
+    private Constants(){}
+}
