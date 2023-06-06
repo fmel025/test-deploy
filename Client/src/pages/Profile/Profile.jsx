eb909366ec75403c7b8deccf4eb5ff7f7b45c55b
Samuel581 <00023020@uca.edu.sv>
2023-06-05 23:48:35 -0600

    :lipstick: Update profile view Updated and finished profile view, improved UI and set the right info to show

--- Client/src/pages/Profile/Profile.jsx
+++ Client/src/pages/Profile/Profile.jsx
@@ -36,12 +36,14 @@ export default function Profile() {
   return (
     <>
       <Layout >
-        <div className="card lg:card-side bg-base-100 shadow-xl m-10 ">
-          <figure><img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=785&q=80" alt="Album" className='h-96' /></figure>
+        <div className="card lg:card-side bg-base-100 shadow-xl my-auto ">
+          <figure><img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=785&q=80" alt="Album" className='h-96 w-96' /></figure>
           <div className="card-body">
             <h2 className="card-title font-bold text-indigo-500">Perfil de usuario</h2>
-            <h3 className=" font-bold text-black mt-5">Usuario</h3>
-            <p className=" font-regular text-blueGray-400">SamCar96</p>
+            <h3 className=" font-bold text-black mt-5">Nombre</h3>
+            <p className=" font-regular text-blueGray-400">
+              Samuel Adonay Ortíz Carballo
+            </p>
             <h2 className=" font-bold text-black">
               Correo electrónico
             </h2>
@@ -51,7 +53,7 @@ export default function Profile() {
             <div className="card-actions justify-start xs:justify-end mt-2">
               <button onClick={() => navigate('/change_password')}
                 className="btn bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ringindigo-300 
-          text-white font-bold"
+          text-white"
               >
                 Cambiar contraseña
               </button>
