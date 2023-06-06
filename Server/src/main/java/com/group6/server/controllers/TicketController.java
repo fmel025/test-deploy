b4a33df93d1a491ffcbd483fab1037ac31687f89
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-06-06 14:50:39 -0600

    :construction: Update Ticket Controller
    It was added the get qr code route for an specific ticket
    
    Co-Authored-By: MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>

--- Server/src/main/java/com/group6/server/controllers/TicketController.java
+++ Server/src/main/java/com/group6/server/controllers/TicketController.java
@@ -1,11 +1,17 @@
 package com.group6.server.controllers;
 
 import com.group6.server.utils.Constants;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(Constants.API_BASE_URL + "/ticket")
 public class TicketController {
-    
+    @GetMapping("/qr/{id}")
+    public ResponseEntity<?> getQRCode(@PathVariable(name = "id") String id){
+        return ResponseEntity.ok().build();
+    }
 }
