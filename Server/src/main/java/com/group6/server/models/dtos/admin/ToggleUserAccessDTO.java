44c864148e1b2d7a71bcd71dab5e4604eead746c
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-06-07 05:12:07 -0600

    :sparkles: Create moderator controller
    It was created the toggle route to ban or unban a certain user
    
    Co-Authored-By: MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>

---
+++ Server/src/main/java/com/group6/server/models/dtos/admin/ToggleUserAccessDTO.java
@@ -0,0 +1,15 @@
+package com.group6.server.models.dtos.admin;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToggleUserAccessDTO {
+
+    @NotEmpty(message = "The user id is required to bam/unban an user")
+    private String userId;
+}
