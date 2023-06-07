a049dd8631fe72eb9046b10180c216898612e161
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-06-07 10:33:41 -0600

    :construction: Edit constants
    The API_ADMIN_URL was changed to private because there will be a controller with the /admin endpoint

--- Server/src/main/java/com/group6/server/utils/Constants.java
+++ Server/src/main/java/com/group6/server/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String API_BASE_URL = "/api/v1";
 
     // For all the workers
-    public static final String API_ADMIN_URL = API_BASE_URL + "/admin";
+    public static final String API_ADMIN_URL = API_BASE_URL + "/private";
 
     // Available for everyone
     public static final String API_USER_URL = API_BASE_URL + "/public";
