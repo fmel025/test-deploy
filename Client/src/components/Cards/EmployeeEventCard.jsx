5de8751a516a61ecc6255f59c20455cfe8dff603
ChristopherCalderon <chris2001289@gmail.com>
2023-06-08 04:11:27 -0600

    :bug: Fix card's shadows
    Fixed card's shadow intensity

--- Client/src/components/Cards/EmployeeEventCard.jsx
+++ Client/src/components/Cards/EmployeeEventCard.jsx
@@ -6,7 +6,7 @@ import { IconTicket } from '@tabler/icons-react';
 
 const EventCard = () => {
     return(
-        <Link to='/employeeeventdetails' className='bg-white cursor-pointer w-72 h-92 border rounded-lg hover:shadow-xl'>
+        <Link to='/employeeeventdetails' className='bg-white cursor-pointer w-72 h-92 border rounded-lg shadow-xl hover:shadow-2xl'>
         <figure className='relative m-3 w-90 h-2/5'>
           <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
         </figure>
