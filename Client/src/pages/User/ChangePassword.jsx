736f1531488a03bed0d60e04ff404fe8a7266371
Samuel581 <00023020@uca.edu.sv>
2023-06-09 06:05:05 -0600

    :lipstick: Upgrade change password view
    Upgraded change password view to work in tiny screen, fixed overlaping
    buttons and adapted form

--- Client/src/pages/User/ChangePassword.jsx
+++ Client/src/pages/User/ChangePassword.jsx
@@ -5,11 +5,7 @@ import ImprovedNavbar from '../../components/Navigation/Navbar/ImprovedNavbar';
 
 function ChangePassword() {
 
-  const navigate = useNavigate();
-  return (
-    <>
-      <Layout>
-        <div className="rounded w-3/5 md:w-3/5 lg:w-2/5 flex-col justify-center p-10 mt-40 bg-white">
+  {/* <div className="rounded w-3/5 md:w-3/5 lg:w-2/5 flex-col justify-center p-10 mt-40 bg-white">
           <h1 className='font-semibold text-3xl text-center mb-10'>Cambiar contraseña</h1>
           <p className='font-semibold'>Nueva Contraseña:</p>
           <input type='password' className='input w-full input-sm bg-slate-200' placeholder='********'></input>
@@ -19,6 +15,31 @@ function ChangePassword() {
             <button className='btn bg-main hover:bg-darker-main text-white font-bold' onClick={() => navigate('/profile')}>Confirmar</button>
             <button className='btn btn-secondary bg-red-500 hover:bg-red-700 text-white font-bold ' onClick={() => navigate('/profile')}>Cancelar</button>
           </div>
+        </div> */}
+
+  const navigate = useNavigate();
+  return (
+    <>
+      <Layout>
+        <div className='card bg-white my-auto'>
+          <div className='card-body'>
+            <h1 className='card-title'>Cambiar contraseña</h1>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Cambiar contraseña</span>
+              </label>
+              <input type='password' className='input w-full input-sm bg-slate-200' placeholder='********'></input>
+              <label className='label'>
+                <span className='label-text'>Cambiar contraseña</span>
+              </label>
+              <input type='password' className='input w-full input-sm bg-slate-200' placeholder='********'></input>
+            </div>
+            <div className='card-actions'>
+              <button className='btn bg-main hover:bg-darker-main text-white font-bold' onClick={() => navigate('/profile')}>Confirmar</button>
+              <button className='btn btn-secondary bg-red-500 hover:bg-red-700 text-white font-bold ' onClick={() => navigate('/profile')}>Cancelar</button>
+            </div>
+          </div>
+
         </div>
       </Layout>
       <ImprovedNavbar />
