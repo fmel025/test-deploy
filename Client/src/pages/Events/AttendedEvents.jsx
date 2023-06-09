61d1c28959e9d1fa3bbf96249c8d8924002f2a2e
Samuel581 <00023020@uca.edu.sv>
2023-06-09 15:06:03 -0600

    :lipstick: Upgrade event statistics view
    Upgraded event statitis view adding a message for mobile devices in
    order to show the chart correctly

--- Client/src/pages/Events/AttendedEvents.jsx
+++ Client/src/pages/Events/AttendedEvents.jsx
@@ -10,7 +10,7 @@ function AttendedEvents() {
     return (
       <>
       <Layout>
-      <h1 className="text-5xl font-bold text-main mb-10 text-center">Eventos asistidos</h1>
+      <h1 className="text-2xl lg:text-5xl font-bold text-main mb-10 text-center">Eventos asistidos</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-16 pb-5">
       <CompletedEventCard />
       <CompletedEventCard />
