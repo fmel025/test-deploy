d83198ea31fb839d89de26f622b83752f9fa57c6
Samuel581 <00023020@uca.edu.sv>
2023-06-09 16:48:25 -0600

    :lipstick: Upgrade transfer ticket view
    Upgraded transfer ticket view to work better in mobile devices

--- Client/src/components/Navigation/Navbar/ImprovedNavbar.jsx
+++ Client/src/components/Navigation/Navbar/ImprovedNavbar.jsx
@@ -29,7 +29,7 @@ const ImprovedNavbar = () => {
           <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
             {
               Links.map((link) => (
-                <li key={link.name} className='md:ml-8 text-lg md:my-0 my-2 hover:bg-slate-300'>
+                <li key={link.name} className='md:ml-8 text-lg md:my-0 my-2 font-bold md:font-normal'>
                   <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
                 </li>
               ))
