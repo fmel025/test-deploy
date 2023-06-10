43938abcce0007afef549917895416979ce944a4
Samuel581 <00023020@uca.edu.sv>
2023-06-09 22:02:37 -0600

    :bug: Fix image shrinks in home
    Fixed both home views where

--- Client/src/pages/Home/EmployeeHome.jsx
+++ Client/src/pages/Home/EmployeeHome.jsx
@@ -3,24 +3,24 @@ import Layout from "../../components/Navigation/Layout/Layout"
 import EmployeeNavbar from "../../components/Navigation/Navbar/EmployeeNavbar"
 
 function EmployeeHome() {
- 
-    return (
-        <>
-        <Layout>
-        <div className="w-full h-full flex items-center justify-between mb-5">
-            <div className="p-36 w-2/4">
-              <h1 className="text-5xl font-bold ">EventMate</h1>
-              <h2 className="text-3xl mt-7">Mas que un evento, es una experiencia, conectate con el mundo</h2>
-            </div>
-            <figure className='relative m-3 w-90 h-full w-2/4'>
-              <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
-            </figure>
+
+  return (
+    <>
+      <Layout>
+        <div className="w-screen flex flex-col lg:flex-row justify-between items-center p-10">
+          <div className="w-4/5 text-center">
+            <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold">EventMate</h1>
+            <h2 className="text-xl md:text-2xl lg:text-3xl mt-7">Mas que un evento, es una experiencia, conectate con el mundo</h2>
+
           </div>
-        </Layout>
-          <EmployeeNavbar/>
-        </>
-    )
-  }
-  
-  export default EmployeeHome
-  
+          <figure className='relative m-3 w-90 h-full md:w-2/4 w-4/5'>
+            <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
+          </figure>
+        </div>
+      </Layout>
+      <EmployeeNavbar />
+    </>
+  )
+}
+
+export default EmployeeHome
