1b033c33fae1f20d8b35c6f4d55bf3b6c8c7f698
Samuel581 <00023020@uca.edu.sv>
2023-06-19 13:23:50 -0600

    :sparkles: Add QR generator
    Added QR generator using react-qr-code package

---
+++ Client/src/pages/Misc/QRScanner.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+import Layout from '../../components/Navigation/Layout/Layout';
+
+function QRScanner() {
+    const [data, setData] = useState('No result');
+
+    const handleScan = (result, error) => {
+        if (result) {
+            setData(result);
+        }
+
+        if (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <>
+            <h1 className='text-2xl text-main font-bold text-center'>QR Scanner</h1>
+            <div className='card w-72 h-fit'>
+                <QrReader onResult={handleScan} />
+            </div>
+
+            <h2 className='text-xl text-main font-bold text-center'>Result</h2>
+            <p>{data}</p>
+        </>
+    );
+}
+
+export default QRScanner;
