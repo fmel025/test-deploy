9854822a1dd34d44863f1b213e54d36cccc887b5
Samuel581 <00023020@uca.edu.sv>
2023-07-01 23:17:42 -0600

    :truck: Add protected routes
    Added protected routes and added roles also separated routes from App.jsx into a routes.jsx file
    Co-authored-by: Fernando Meléndez <00018720@uca.edu.sv>

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,79 +1,8 @@
-import { useRoutes, BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import AssistedEvents from "./pages/Events/AttendedEvents";
-import FutureEvents from "./pages/Events/FutureEvents";
-import EventDetails from "./pages/Events/EventDetails";
-import EmployeeEventDetails from "./pages/Events/EmployeeEventDetails";
-import PurchaseTicket from "./pages/Misc/PurchaseTicket";
-import Transactions from "./pages/Misc/Transactions";
-import Profile from "./pages/User/Profile";
-import ClientNavbar from "./components/Navigation/Navbar/ImprovedNavbar";
-import EmployeeHome from "./pages/Home/EmployeeHome";
-import ManageEvents from "./pages/User/ManageEvents";
-import EventStatistics from "./pages/Events/EventStatistics";
-import EventStatisticsSelection from "./pages/Events/EventStatisticsSelection";
-import Login from "./pages/User/Login";
-import ChangePassword from "./pages/User/ChangePassword";
-import Transaction from "./pages/Misc/Transaction";
-import EditEvent from "../src/components/formsContent/EditEvent";
-import ManageUsers from "./pages/User/ManageUsers";
-import ManageScanners from "./pages/User/ManageScanners";
-import ManageAuthorizations from "./pages/User/ManageAuthorizations";
-import PurchaseList from "./pages/User/PurchaseList";
-import OrderDetails from "./pages/Misc/OrderDetails";
-import TransactionDetails from "./pages/Misc/TransactionDetails";
-import CreateEventForm from "./pages/Events/CreateEventForm";
-import EditCategoriesForm from "./components/formsContent/editEvent/EditCategoriesForm";
-import EditOrganizersForm from "./components/formsContent/editEvent/EditOrganizersForm";
-import EditTiersForm from "./components/formsContent/editEvent/EditTiersForm";
-import QRScanner from "./pages/Misc/QRScanner";
-import EditSponsorsForm from "./components/formsContent/editEvent/EditSponsorsForm";
+import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import PrivateRoute from "./utils/PrivateRoute";
 import AuthProvider from "../src/context/AuthContext";
+import AppRoutes from "./routes";
 
-const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/private/home" element={<EmployeeHome />} />
-    <Route path="/scanner" element={<QRScanner />} />
-    <Route path="/event/attended" element={<AssistedEvents />} />
-    <Route path="/event/future" element={<FutureEvents />} />
-    <Route path="/event/details/id" element={<EventDetails />} />
-    <Route
-      path="/private/event/details/id"
-      element={<EmployeeEventDetails />}
-    />
-    <Route
-      path="/event/statistics"
-      element={
-        <PrivateRoute roles={["WORKER"]}>
-          <EventStatisticsSelection />
-        </PrivateRoute>
-      }
-    />
-    <Route path="/event/statistics/id" element={<EventStatistics />} />
-    <Route path="/event/manage" element={<ManageEvents />} />
-    <Route path="/event/details/id/purchase/id" element={<PurchaseTicket />} />
-    <Route path="/purchase" element={<PurchaseList />} />
-    <Route path="/purchase/id" element={<OrderDetails />} />
-    <Route path="/transactions" element={<Transactions />} />
-    <Route path="/manage/users" element={<ManageUsers />} />
-    <Route path="/manage/scanners" element={<ManageScanners />} />
-    <Route path="/manage/auth" element={<ManageAuthorizations />} />
-    <Route path="/user/profile" element={<Profile />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/user/setpassword" element={<ChangePassword />} />
-    <Route path="/transaction" element={<Transaction />} />
-    <Route path="/transaction/id" element={<TransactionDetails />} />
-    <Route path="/event/create" element={<CreateEventForm />} />
-    <Route path="/edit/event/id" element={<EditEvent />} />
-    <Route path="/edit/categories/id" element={<EditCategoriesForm />} />
-    <Route path="/edit/organizers/id" element={<EditOrganizersForm />} />
-    <Route path="/edit/tiers/id" element={<EditTiersForm />} />
-    <Route path="/edit/sponsors/id" element={<EditSponsorsForm />} />
-  </Routes>
-);
 
 function App() {
   return (
---
+++ Client/src/routes.jsx
@@ -0,0 +1,215 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AssistedEvents from "./pages/Events/AttendedEvents";
+import FutureEvents from "./pages/Events/FutureEvents";
+import EventDetails from "./pages/Events/EventDetails";
+import EmployeeEventDetails from "./pages/Events/EmployeeEventDetails";
+import PurchaseTicket from "./pages/Misc/PurchaseTicket";
+import Transactions from "./pages/Misc/Transactions";
+import Profile from "./pages/User/Profile";
+import EmployeeHome from "./pages/Home/EmployeeHome";
+import ManageEvents from "./pages/User/ManageEvents";
+import EventStatistics from "./pages/Events/EventStatistics";
+import EventStatisticsSelection from "./pages/Events/EventStatisticsSelection";
+import Login from "./pages/User/Login";
+import ChangePassword from "./pages/User/ChangePassword";
+import Transaction from "./pages/Misc/Transaction";
+import EditEvent from "../src/components/formsContent/EditEvent";
+import ManageUsers from "./pages/User/ManageUsers";
+import ManageScanners from "./pages/User/ManageScanners";
+import ManageAuthorizations from "./pages/User/ManageAuthorizations";
+import PurchaseList from "./pages/User/PurchaseList";
+import OrderDetails from "./pages/Misc/OrderDetails";
+import TransactionDetails from "./pages/Misc/TransactionDetails";
+import CreateEventForm from "./pages/Events/CreateEventForm";
+import EditCategoriesForm from "./components/formsContent/editEvent/EditCategoriesForm";
+import EditOrganizersForm from "./components/formsContent/editEvent/EditOrganizersForm";
+import EditTiersForm from "./components/formsContent/editEvent/EditTiersForm";
+import QRScanner from "./pages/Misc/QRScanner";
+import EditSponsorsForm from "./components/formsContent/editEvent/EditSponsorsForm";
+import PrivateRoute from "./utils/PrivateRoute";
+
+const AppRoutes = () => (
+    <Routes>
+        <Route
+            path="/"
+            element={<Home />} />
+        <Route
+            path="/private/home"
+            element={
+                <PrivateRoute roles={["WORKER", "ADMIN"]}>
+                    <EmployeeHome />
+                </PrivateRoute>
+            } />
+        <Route
+            path="/scanner"
+            element={
+                <PrivateRoute roles={["ADMIN", "VALIDATOR"]}>
+                    <QRScanner />
+                </PrivateRoute>} />
+
+        <Route
+            path="/event/attended"
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <AssistedEvents />
+                </PrivateRoute>} />
+        <Route
+            path="/event/future"
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <FutureEvents />
+                </PrivateRoute>} />
+        {/* Leave this public */}
+        <Route path="/event/details/id" element={<EventDetails />} />
+        <Route
+            path="/private/event/details/id"
+            element={<EmployeeEventDetails />}
+        />
+        <Route
+            path="/event/statistics"
+            element={
+                <PrivateRoute roles={["ANALYST", "ADMIN"]}>
+                    <EventStatisticsSelection />
+                </PrivateRoute>
+            }
+        />
+        <Route 
+        path="/event/statistics/id" 
+        element={
+            <PrivateRoute roles={["ANALYST", "ADMIN"]}>
+                <EventStatistics />
+            </PrivateRoute>
+        } />
+        <Route 
+        path="/event/manage" 
+        element={
+            <PrivateRoute roles={["ADMIN", "EVENT_MOD"]}>
+                <ManageEvents />
+            </PrivateRoute>
+        } />
+        <Route 
+            path="/event/details/id/purchase/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <PurchaseTicket />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/purchase" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <PurchaseList />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/purchase/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <OrderDetails />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/transactions" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <Transactions />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/manage/users" 
+            element={
+                <PrivateRoute roles={["ADMIN", "MOD"]}>
+                    <ManageUsers />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/manage/scanners" 
+            element={
+                <PrivateRoute roles={["ADMIN", "MOD"]}>
+                    <ManageScanners />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/manage/auth" 
+            element={
+                <PrivateRoute roles={["ADMIN"]}>
+                    <ManageAuthorizations />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/user/profile" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT", "WORKER"]}>
+                    <Profile />
+                </PrivateRoute>
+            } />
+        <Route path="/login" element={<Login />} />
+        <Route 
+            path="/user/setpassword" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT", "WORKER"]}>
+                    <ChangePassword />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/transaction" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <Transaction />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/transaction/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "CLIENT"]}>
+                    <TransactionDetails />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/event/create" 
+            element={
+                <PrivateRoute roles={["ADMIN", "EVENT_MOD", "WORKER"]}>
+                    <CreateEventForm />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/edit/event/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "EVENT_MOD"]}>
+                    <EditEvent />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/edit/categories/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "EVENT_MOD"]}>
+                    <EditCategoriesForm />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/edit/organizers/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "EVENT_MOD"]}>
+                    <EditOrganizersForm />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/edit/tiers/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "EVENT_MOD"]}>
+                    <EditTiersForm />
+                </PrivateRoute>
+            } />
+        <Route 
+            path="/edit/sponsors/id" 
+            element={
+                <PrivateRoute roles={["ADMIN", "EVENT_MOD"]}>
+                    <EditSponsorsForm />
+                </PrivateRoute>
+            } />
+    </Routes>
+);
+
+
+export default AppRoutes;
