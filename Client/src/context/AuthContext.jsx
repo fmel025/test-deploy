da7935f05716ecc464d7bcd55e7072246f5f6c40
Fernando Meléndez <62614716+fmel025@users.noreply.github.com>
2023-07-02 09:36:08 -0600

    :bug: Fix routes losing perms

--- Client/src/context/AuthContext.jsx
+++ Client/src/context/AuthContext.jsx
@@ -5,12 +5,13 @@ import axios from "../utils/axios";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-
     const [token, setToken] = useState(() => {
         return window.sessionStorage.getItem('token') || null;
     });
 
-    const [permissions, setPermissions] = useState([]);
+    const [permissions, setPermissions] = useState(() => {
+        return JSON.parse(window.sessionStorage.getItem('permissions')) || [];
+    });
 
     const [isPasswordSet, setIsPasswordSet] = useState(false);
 
@@ -21,15 +22,14 @@ const AuthProvider = ({ children }) => {
                     Authorization: `Bearer ${token}`
                 }
             }).then((response) => {
+                window.sessionStorage.setItem('permissions', JSON.stringify(response.data.permissions));
                 setPermissions(response.data.permissions);
                 setIsPasswordSet(response.data.isPasswordSet);
-            }
-            ).catch((error) => {
+            }).catch((error) => {
                 console.log(error);
-            }
-            );
+            });
         }
-    }, [token])
+    }, [token]);
 
     return (
         <AuthContext.Provider value={{ token, setToken, permissions, setPermissions, isPasswordSet, setIsPasswordSet }}>
--- Client/src/utils/PrivateRoute.jsx
+++ Client/src/utils/PrivateRoute.jsx
@@ -2,20 +2,30 @@ import { Route, Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 const PrivateRoute = ({ roles, children }) => {
-  const { isLoggedIn, permissions } = useAuth();
+  const { isLoggedIn, permissions, token } = useAuth();
 
-  if (!isLoggedIn()) {
+  // If user is not logged in, navigate to login page
+  if (!isLoggedIn) {
     return <Navigate to="/login" />;
   }
 
-  if (permissions && !roles.some((r) => permissions.includes(r))) {
-    if (!permissions.includes("WORKER")) {
-      return <Navigate to="/" />;
-    }
+  console.log(token);
 
-    return <Navigate to="/private/home" />;
+  console.log(permissions);
+  // If user does not have necessary roles, navigate them accordingly
+  if (!roles.some(role => permissions.map(p => p.trim().toUpperCase()).includes(role.trim().toUpperCase()))) {
+    
+    console.log(permissions);
+
+    if (permissions.map(p => p.trim().toUpperCase()).includes("WORKER")) {
+      return <Navigate to="/worker-home" />;
+    }
+    
+    // Navigate to root as default
+    return <Navigate to="/" />;
   }
 
+  // If user is logged in and has the necessary role(s), render the children
   return children;
 };
 
